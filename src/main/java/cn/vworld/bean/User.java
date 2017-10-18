4bd334b8596e24524d5f91042a01e26e585b9ff3
taieo <13906569268@163.com>
2017-10-18 12:46:27 +0800

    bean_中途版本

--- src/main/java/cn/vworld/bean/User.java
+++ src/main/java/cn/vworld/bean/User.java
@@ -1,10 +1,49 @@
 package cn.vworld.bean;
 
-public class User {
+public class User extends BaseEntity{
     private String userId;      //用户ID
     private String username;    //用户名
     private String password;    //用户密码
-    private int state;          //用户状态 邮箱已验证为1，否则为0
+    private Integer state;          //用户状态 邮箱已验证为1，否则为0
+    private Integer ban;        //  1代表禁用',0启用
 
+    public String getUserId() {
+        return userId;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Integer getBan() {
+        return ban;
+    }
+
+    public void setBan(Integer ban) {
+        this.ban = ban;
+    }
 }
