54ec2b269304f55e0fb0661b7dbe1c12bc3e321b
taieo <13906569268@163.com>
2017-10-18 09:58:28 +0800

    1

--- src/main/java/cn/vworld/controller/TestController.java
+++ src/main/java/cn/vworld/controller/TestController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Date;
 
 @Controller
-public class UserController {
+public class TestController {
 
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
 
+        return "hello,现在是"+new Date().toLocaleString();
+    }
 
 }
