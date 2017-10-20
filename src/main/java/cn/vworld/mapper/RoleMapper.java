8a2377277a43505a084a736de81476ba1710a705
253906704 <253906704@qq.com>
2017-10-20 16:55:07 +0800

    role 增删改查   day03 16:54

---
+++ src/main/java/cn/vworld/mapper/RoleMapper.java
@@ -0,0 +1,18 @@
+package cn.vworld.mapper;
+
+import cn.vworld.bean.Role;
+
+import java.util.List;
+
+public interface RoleMapper {
+
+    List<Role> findAll();
+
+    void saveRole(Role role);
+
+    void deleteRoleById_m(String roleId);
+
+    void deleteRoleById(String roleId);
+
+    void updateRoleById(String roleId);
+}
