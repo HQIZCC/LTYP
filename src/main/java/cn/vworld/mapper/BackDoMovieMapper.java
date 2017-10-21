e04347d60a519d745d2d2b80a47f7ff9a1ab0ef6
HQIZCC <1358805979@qq.com>
2017-10-21 18:29:18 +0800

    2017-10-20 1. movieInfo修改----三张图片显示 2. deleleMovie/deleteMovieImage 关联删除 3. updatemovieInfo -- 同时做到海报修改，详情页1/2修改（不修改也没关系）--- 修改的同时、做到脏数据的修改 4. linuxmysql环境搭建 5. linux下的mysql字符集修改，解决乱码问题latain1 to utf-8

--- src/main/java/cn/vworld/mapper/BackDoMovieMapper.java
+++ src/main/java/cn/vworld/mapper/BackDoMovieMapper.java
@@ -1,7 +1,30 @@
 package cn.vworld.mapper;
 
+import cn.vworld.bean.MovieImage;
 import cn.vworld.bean.MovieInfo;
+
+import java.util.List;
 
 public interface BackDoMovieMapper {
     public MovieInfo findMovieInfoByMovieId(String movieId);
+
+    List<MovieImage> findMovieImageByMovieId(String movieId);
+
+    /**
+     * 修改提交后的入库操作，需要先将对应movieId的MovieInfo删除
+     *
+     * @param movieId
+     */
+    void deleteMovieInfoByMovieId(String movieId);
+
+    /**
+     * 修改提交前，先根据movieID删除movieImage实例
+     *
+     * @param movieId
+     */
+    void deleteMovieImageByMovieId(String movieId);
+
+    void deleteMovieInfoByMovieIds(String[] movieIds);
+
+    void deleteMovieImageByMovieIds(String[] movieIds);
 }
--- src/main/webapp/WEB-INF/backend/movieList.jsp
+++ src/main/webapp/WEB-INF/backend/movieList.jsp
@@ -184,6 +184,10 @@
 
                 <div class="am-btn-group am-btn-group-xs">
                     <a href="#" class="am-btn am-btn-default" onclick="formSubmit('toview','_self');this.blur();"><span class="am-icon-plus"></span> 查看</a>
+                    <a href="#" class="am-btn am-btn-default"
+                       onclick="formSubmit('toupdate','_self');this.blur();"><span class="am-icon-plus"></span> 修改</a>
+                    <a href="#" class="am-btn am-btn-default"
+                       onclick="formSubmit('todelete','_self');this.blur();"><span class="am-icon-plus"></span> 删除</a>
 
                 </div>
 
